e6ea9652bf479343e8d3435cee2b0a8b93f9b3cd
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2022-07-22 22:33:21 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Spinner from "react-spinkit";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "./Login";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
+            alt=""
+          />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
   return (
     <div className="app">
       <Router>
@@ -34,6 +48,26 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
 
 const AppBody = styled.div`
   display: flex;
